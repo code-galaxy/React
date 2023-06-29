8b5dc8bf269f186fab50ceadb92b009c587d3058
Yaroslav Karpash <ykarpash@gmail.com>
2023-06-29 14:31:12 +0300

    Add files via upload

---
+++ unit_1 - First react app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+let text = 'test variable';
+
+
+
+function App() {
+  let text2 = '222';
+
+  const style = {
+    fontSize: '24px',
+    fontStyle: 'italic',
+    color: 'red'
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1 style={style}>app_1</h1>
+        <img src="/images/reactmini.webp" alt="reactmini" />
+        <ul>
+          <li>Hello</li>
+          <li>{text + text2}</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
